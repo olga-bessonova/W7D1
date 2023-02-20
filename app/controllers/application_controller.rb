0d20c4d6011c4d03a3e8a9625d79067e6f033b83
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-20 17:27:07 -0500

    views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    # skip_before_action :verify_authenticity_token
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
 end
